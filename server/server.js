1ae611376fe9fc6b91d478f51b2b24f6e03fa8cd
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-06-23 15:24:40 +1200

    setup apiclient and server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const express = require('express')
 
+const cocktailbar = require('./routes/cocktailbar')
+
 const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/cocktailbar', cocktailbar)
 
 module.exports = server
