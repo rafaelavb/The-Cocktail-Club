e1544163cd8a713d5bb28cc23ebbf1b7a8234e4a
Mat Ruane <mat@ruane.net.nz>
2022-06-23 20:25:26 +1200

    Renders Ingredient(s) with dummy props

---
+++ client/components/Ingredients.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { getIngredientByName } from './apiClient'
+
+// dummy props
+const ingredientsArr = ['Tequila', 'Triple sec', 'Lime juice', 'Salt']
+
+function Ingredient(props) {
+  const { name } = props
+  const [details, setDetails] = useState({ loading: true })
+  const [showDesc, setShowDesc] = useState(false)
+
+  useEffect(() => {
+    getIngredientByName(name)
+      .then((data) => {
+        setDetails({ data: data.ingredients[0] })
+      })
+      .catch((err) => {
+        setDetails({ error: err.message })
+      })
+  }, [])
+
+  const handleClick = (evt) => {
+    setShowDesc((prev) => !prev)
+  }
+
+  if (details.loading) {
+    return <p>Loading...</p>
+  }
+
+  if (details.error) {
+    return <p>Something went wrong</p>
+  }
+
+  return (
+    <div>
+      {console.log(details)}
+      <h3>{details.data.strIngredient}</h3>
+      <p>Type: {details.data.strType}</p>
+      <p>Alcoholic: {details.data.strAlcohol}</p>
+      <button onClick={handleClick}>Description</button>
+      {showDesc && <p>{details.data.strDescription}</p>}
+    </div>
+  )
+}
+
+function Ingredients(props) {
+  // const {ingredientsArr} = props
+
+  return (
+    <ul>
+      {ingredientsArr.map((ingredient) => (
+        <Ingredient key={ingredient} name={ingredient} />
+      ))}
+    </ul>
+  )
+}
+
+export default Ingredients
--- client/components/apiClient.js
+++ client/components/apiClient.js
@@ -1,8 +1,16 @@
 import request from 'superagent'
 
-const serverURL = 'http://localhost:3000/api/v1'
+const serverURL = 'https://www.thecocktaildb.com/api/json/v1/1/'
 
-export function getCocktailApi(){
-  return request.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`).then((response)=>response.body)
+export function getCocktailApi() {
+  return request
+    .get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`)
+    .then((response) => response.body)
 }
 
+export function getIngredientByName(name) {
+  console.log('hi')
+  return request
+    .get(`${serverURL}search.php?i=${name}`)
+    .then((response) => response.body)
+}
