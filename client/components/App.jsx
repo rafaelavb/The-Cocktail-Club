cd9545059c27fd4b41e715dfa38ff923d3780c4b
Rafael <103238522+rafaelavb@users.noreply.github.com>
2022-06-24 10:57:06 +1200

    added random onclick function to nav link random cocktail

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { getRandomCocktailApi } from './apiClient'
 
 import Nav from './Nav'
 import Home from './Home'
@@ -11,15 +12,35 @@ import RandomCocktail from './RandomCocktail'
 import PageNotFound from './PageNotFound'
 
 function App() {
+  const [cocktail, setCocktail] = useState({ loading: true })
+  useEffect(() => {
+    getRandomCocktail()
+  }, [])
+
+  const getRandomCocktail = () => {
+    getRandomCocktailApi()
+      .then((data) => {
+        console.log(data, `im the data`)
+        setCocktail({ data: data.drinks })
+      })
+      .catch((err) => {
+        console.log(err.message)
+        setCocktail({ error: err.message })
+      })
+  }
+
   return (
     <>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className="main">
         <h1>Welcome to The Cocktail Club</h1>
-        <Nav />
+        <Nav fetchCocktail={getRandomCocktail} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/randomcocktail" element={<RandomCocktail />} />
+          <Route
+            path="/randomcocktail"
+            element={<RandomCocktail cocktail={cocktail} />}
+          />
           <Route path="/alcoholic" element={<Alcoholic />} />
           <Route path="/ingredients" element={<Ingredients />} />
           <Route path="/non-alcoholic" element={<NonAlcoholic />} />
