fca64f036f3230336eecdc77b92e34e9a1761a1f
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-06-23 20:47:34 +1200

    non-alch components done

--- client/components/apiClient.js
+++ client/components/apiClient.js
@@ -2,7 +2,14 @@ import request from 'superagent'
 
 const serverURL = 'http://localhost:3000/api/v1'
 
-export function getCocktailApi(){
-  return request.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`).then((response)=>response.body)
+export function getCocktailApi() {
+  return request
+    .get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`)
+    .then((response) => response.body)
 }
 
+export function getNonAlcoholic() {
+  return request
+    .get('www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
+    .then((response) => response.body)
+}
