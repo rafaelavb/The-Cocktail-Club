cadce714452c3ce1bd5ce88ab8f2be7c092221d2
Vincent Nuku <103153767+VincentNuku@users.noreply.github.com>
2022-06-23 17:04:49 +1200

    Alcoholics component

--- client/components/apiClient.js
+++ client/components/apiClient.js
@@ -1,8 +1,8 @@
-import { response } from 'express'
 import request from 'superagent'
 
 const serverURL = 'http://localhost:3000/api/v1'
 
 export function getCocktailApi(){
-  return request.get(`${serverURL}/cocktailbar`).then((response)=>response.body)
+  return request.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`).then((response)=>response.body)
 }
+
