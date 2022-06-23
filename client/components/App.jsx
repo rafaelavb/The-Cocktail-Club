6ccb4f4dfed539951e31bc9bfc5cfd88946dd285
Mat Ruane <mat@ruane.net.nz>
2022-06-24 07:08:31 +1200

    fix merge issues

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,21 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+
 import Nav from './Nav'
 import Home from './Home'
 import LiquorType from './LiquorType'
-
-
-
-import { Routes, Route } from 'react-router-dom'
 import Alcoholic from './Alcoholic'
 import NonAlcoholic from './NonAlcoholic'
 import Ingredients from './Ingredients'
-
-
-
-
 import RandomCocktail from './RandomCocktail'
-import { getNonAlcoholic } from './apiClient'
-
 
 function App() {
   return (
     <>
-
-      
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className="main">
+        <h1>Welcome to The Cocktail Club</h1>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/randomcocktail" element={<RandomCocktail />} />
@@ -33,15 +24,6 @@ function App() {
           <Route path="/non-alcoholic" element={<NonAlcoholic />} />
           <Route path="/liquortype" element={<LiquorType />} />
         </Routes>
-
-      <h1>Welcome to The Cocktail Club</h1>
-
-      {/* This 'main' div is only for styling (so we can use flexbox) */}
-      
-        <Home />
-        <Nav />
-        
-
       </div>
     </>
   )
