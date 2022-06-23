4af26f3aca2dbb6fe11cfdee64451a8b21585b15
Mat Ruane <8108538+matruane@users.noreply.github.com>
2022-06-23 19:40:21 +1200

    solved merging issues

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -7,29 +7,26 @@ function Nav() {
       <h2>Browse by...</h2>
       <ul>
         <li>
-          <Link to={'/allcocktails'}>{All Cocktails}</Link>
+          <Link to={'/alcoholic'}>{'Alcoholic'}</Link>
         </li>
         <li>
-          <Link to={'/alcoholic'}>{Alcoholic}</Link>
-        </li>    
-        <li>
-          <Link to={'/non-alcoholic'}>{Non-Alcoholic}</Link></li>
-        <li>
-          <Link to={'/ingredients'}>{Ingredients}</Link>
+          <Link to={'/non-alcoholic'}>{'Non-Alcoholic'}</Link>
         </li>
         <li>
-          <Link to={'/glasstype'}>{Glass Type}</Link>
+          <Link to={'/ingredients'}>{'Ingredients'}</Link>
         </li>
         <li>
-          <Link to={'/liquortype'}>{Liquor Type}</Link>
+          <Link to={'/glasstype'}>{'Glass Type'}</Link>
         </li>
         <li>
-          <Link to={'/randomcocktail'}>{Random Cocktail}</Link>
+          <Link to={'/liquortype'}>{'Liquor Type'}</Link>
+        </li>
+        <li>
+          <Link to={'/randomcocktail'}>{'Random Cocktail'}</Link>
         </li>
       </ul>
     </div>
   )
 }
-
 
 export default Nav
