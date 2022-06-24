adbe9e515df1d924bd75ee1d33d52ebeab3ede8b
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-06-24 13:08:10 +1200

    link on alch page

--- client/components/Alcoholic.jsx
+++ client/components/Alcoholic.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { getAlcoholic } from './apiClient'
+import DrinkDetail from './DrinkDetail'
 
 function Alcoholic() {
   const [drinks, setDrinks] = useState({ loading: true })
